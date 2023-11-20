9e237a72d3d2888fbf344c5fed64df17d696e3c5
+ <+>
2023-11-20 12:02:42 +0300

    working on components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import BotCollection from './components/BotCollection';
+import YourBotArmy from './components/YourBotArmy';
 
 function App() {
   return (
---
+++ src/components/YourBotArmy.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+function YourBotArmy  ({ army, onReleaseFromArmy, onDischarge })  {
+    return (
+      <div>
+        <h2>Your Bot Army</h2>
+        {army.map((bot) => (
+          <div key={bot.id}>
+            <BotProfile bot={bot} />
+            <button onClick={() => onReleaseFromArmy(bot)}>Release from Army</button>
+            <button onClick={() => onDischarge(bot.id)}>Discharge</button>
+          </div>
+        ))}
+      </div>
+    );
+  };
+
+  export default YourBotArmy;
