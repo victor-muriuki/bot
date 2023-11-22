aa7ad6c267e35994207b769b258785bd68a99b1a
+ <+>
2023-11-22 05:39:38 +0300

    completed assignment

--- src/App.js
+++ src/App.js
@@ -1,27 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 import BotCollection from './components/BotCollection';
 import YourBotArmy from './components/YourBotArmy';
 
-function App() {
+function App  () {
+  const [bots, setBots] = useState([]);
+  const [army, setArmy] = useState([]);
+
+  useEffect(() => {
+   
+    fetch('http://127.0.0.1:3000/bots')
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setBots(data);
+      })
+      .catch((error) => {
+        console.error('Error fetching bots:', error);
+      });
+  }, []); 
+
+  const addToArmy = (selectedBot) => {
+    if (!army.includes(selectedBot)) {
+      setArmy((prevArmy) => [...prevArmy, selectedBot]);
+    }
+  };
+
+  const releaseFromArmy = (releasedBot) => {
+    setArmy((prevArmy) => prevArmy.filter((bot) => bot !== releasedBot));
+  };
+
+  const dischargeBot = (botId) => {
+    
+    setArmy((prevArmy) => prevArmy.filter((bot) => bot.id !== botId));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>War Bots</h1>
+
+      <div>
+        <h2>Bot Collection</h2>
+        <BotCollection bots={bots} onAddToArmy={addToArmy} />
+      </div>
+
+      <div>
+        <h2>Your Bot Army</h2>
+        <YourBotArmy army={army} onReleaseFromArmy={releaseFromArmy} onDischarge={dischargeBot} />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/BotProfile.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function BotProfile  ({ bot }) {
+    return (
+      <div>
+        <img src={bot.avatar_url} alt={bot.name} />
+        <p>Name: {bot.name}</p>
+        <p>Health: {bot.health}</p>
+        {/* Include other bot details */}
+      </div>
+    );
+  };
+
+  export default BotProfile;
